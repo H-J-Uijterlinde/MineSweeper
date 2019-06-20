6fd9d91a8f175943d18d5d41ee7c92bc794f9b27
HJU <hjuijterlinde@hotmail.com>
2019-06-20 13:19:24 +0200

    Added a comment to the main class.

--- src/main/java/com/minesweeper/RunMineSweeper.java
+++ src/main/java/com/minesweeper/RunMineSweeper.java
@@ -4,6 +4,14 @@ import com.minesweeper.GameUtils.DifficultyLevel;
 import com.minesweeper.UserInterface.GameInterface;
 import com.minesweeper.gamefield.GameField;
 
+/*
+This class contains the main method, which is the entry point of the application. It creates
+the first GameField, for which is chose beginner as the default difficulty level. It then prints
+the GameField object to the console, this obviously isn't necessary, but I left it in for
+debugging at first, now mainly for illustration purposes. Finally it creates a new GameInterface
+which is the GUI object of the application.
+ */
+
 public class RunMineSweeper {
 
     public static void main(String[] args) {
@@ -11,6 +19,6 @@ public class RunMineSweeper {
 
         System.out.println(newGame.toString());
 
-        GameInterface startGame = new GameInterface(newGame);
+        new GameInterface(newGame);
     }
 }
